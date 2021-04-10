130d1916110cb4e533f2e0ff7ab08fe4307effaa
Prasanna Mayilvahanan <prasanna10294@gmail.com>
2021-04-10 08:52:37 +0200

    Delete __init__.py

--- vision_transformer_pytorch/__init__.py
+++
@@ -1,7 +0,0 @@
-__version__ = "1.0.3"
-from .model import VisionTransformer, VisionTransformer_qk_fixed, VALID_MODELS
-from .utils import (
-    Params,
-    vision_transformer,
-    get_model_params,
-)
